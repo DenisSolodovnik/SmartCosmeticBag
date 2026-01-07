eca2e3c8ef9d64fefd54f24c54bb12d26f040996
Denis Solodovnik <garbboxg@icloud.com>
2026-01-07 20:08:50 +0700

    [WIP]

---
+++ SmartCosmeticBag/SplashScreen.swift
@@ -0,0 +1,15 @@
+//
+//  SplashScreen.swift
+//  SmartCosmeticBag
+//
+//  Created by Денис Солодовник on 07.01.2026.
+//
+
+import SwiftUI
+
+struct SplashScreen: View {
+
+    var body: some View {
+        Text("Splash Screen")
+    }
+}
